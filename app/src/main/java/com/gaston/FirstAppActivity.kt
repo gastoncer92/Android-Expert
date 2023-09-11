9bd1cf84d263494eaa1de90a0298198a942b0c25
gastoncer92 <kaupisoft@gmail.com>
2023-09-11 14:54:39 -0300

    weeefef

---
+++ app/src/main/java/com/gaston/FirstAppActivity.kt
@@ -0,0 +1,12 @@
+package com.gaston
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aristidevs.androidmaster.R
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_app2)
+    }
+}

2d65f8135023271c057e86596b04f031e35b2858
gastoncer92 <kaupisoft@gmail.com>
2023-09-12 14:59:37 -0300

    fgdfbfdb
    dfbdfb

--- app/src/main/java/com/gaston/FirstAppActivity.kt
+++ app/src/main/java/com/gaston/FirstAppActivity.kt
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.aristidevs.androidmaster.R
 
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app2)
-
         val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName1 = findViewById<AppCompatEditText>(R.id.etName1)
+
+
+
         btnStart.setOnClickListener {
-            Log.i("AristiDevs", "Boton precionado")
+            Log.i("AristiDevs", "Boton precionado ${etName1.text.toString()}")
         }
+
+
+
+
     }
 }

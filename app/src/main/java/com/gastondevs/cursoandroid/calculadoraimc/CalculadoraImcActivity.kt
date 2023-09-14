c356da3e76c94d1245fa4e5505d10a9707ce87e0
Gastón <kaupisoft@gmail.com>
2023-09-14 00:57:57 -0300

    awdawd
    awdawd

--- app/src/main/java/com/gastondevs/cursoandroid/calculadoraimc/CalculadoraImcActivity.kt
+++ app/src/main/java/com/gastondevs/cursoandroid/calculadoraimc/CalculadoraImcActivity.kt
@@ -1,10 +1,14 @@
 package com.gastondevs.cursoandroid.calculadoraimc
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import com.aristidevs.androidmaster.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.RangeSlider
+import java.text.DecimalFormat
 
 class CalculadoraImcActivity : AppCompatActivity() {
 
@@ -13,6 +17,10 @@ class CalculadoraImcActivity : AppCompatActivity() {
 
     private lateinit var viewMale: CardView
     private lateinit var viewFemale: CardView
+    private lateinit var tvHeight: TextView
+    private lateinit var rsHeight: RangeSlider
+    private lateinit var btnSubtractWeight: FloatingActionButton
+    private lateinit var btnPlusWeight: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,13 +31,33 @@ class CalculadoraImcActivity : AppCompatActivity() {
     }
 
     private fun initListeners() {
-        viewMale.setOnClickListener { getBackgroundColor(isMaleSelected) }
-        viewFemale.setOnClickListener { getBackgroundColor(isMaleSelected) }
+        viewMale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+
+        viewFemale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+
+        rsHeight.addOnChangeListener { _, value, _ ->
+            val df = DecimalFormat("#.##")
+            val result = df.format(value)
+            tvHeight.text = "$result cm"
+        }
+    }
+
+    private fun changeGender() {
+        isMaleSelected = !isMaleSelected
+        isFemaleSelected = !isFemaleSelected
     }
 
     private fun initComponent() {
         viewMale = findViewById(R.id.viewMale)
         viewFemale = findViewById(R.id.viewFemale)
+        tvHeight = findViewById(R.id.tvHeight)
+        rsHeight = findViewById(R.id.rsHeight)
     }
 
     private fun setGenderColor() {
